e97e908c918615e8f797b0bf5af2865d6049ad16
Tiago de Oliveira <deolivtiago@outlook.com>
2024-10-22 21:33:37 +1100

    feat: add schema to validate user attrs

--- lib/clarx_core.ex
+++ lib/clarx_core.ex
@@ -5,5 +5,31 @@ defmodule ClarxCore do
 
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
+
+  This can be used in your application as:
+      use ClarxCore, :schema
   """
+
+  def schema do
+    quote do
+      use Ecto.Schema
+
+      import Ecto.Changeset
+
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+      @timestamps_opts type: :utc_datetime
+
+      def changeset, do: %{change(struct(__MODULE__)) | valid?: false}
+      def changeset(schema) when is_struct(schema, __MODULE__), do: change(schema)
+
+      defp cast(schema, attrs) when is_non_struct_map(schema) or is_list(schema) do
+        %{Goal.build_changeset(schema, attrs) | valid?: false}
+      end
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
 end
